7157d66ef883f5de2f9407f452ad6ceab0ca8109
ray glover <ray@rayglover.net>
2015-04-25 12:46:53 +0100

    merged with existing vec/set impl.

--- benches/lib.rs
+++ benches/lib.rs
@@ -1,23 +1,35 @@
 #![feature(test)]
 #![feature(collections)]
+#![feature(core)]
 
 extern crate test;
 extern crate rand;
 extern crate faster_bitset;
 
-use std::collections::BitSet;
-use test::Bencher;
+use std::{u32};
+use rand::*;
+use test::{Bencher, black_box};
 
-use faster_bitset::*;
 
-static N:usize = 1000000;
+#[cfg(feature = "std")] use std::collections::{BitSet, BitVec};
+#[cfg(not(feature = "std"))] use faster_bitset::{BitSet, BitVec};
+
+const N : usize = 1000000;
+const BENCH_BITS : usize = 1 << 14;
+
+
+fn rng() -> rand::IsaacRng {
+    let seed: &[_] = &[1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    rand::SeedableRng::from_seed(seed)
+}
 
 fn create_rnd(n: usize, p:f32) -> (BitSet, usize) {
+    let mut r = rng();
     let mut s = BitSet::with_capacity(n);
     let mut k = 0;
 
     for i in 0..n-1 {
-        if rand::random::<f32>() < p {
+        if r.next_f32() < p {
             s.insert(i);
             k += 1;
         }
@@ -25,6 +37,7 @@ fn create_rnd(n: usize, p:f32) -> (BitSet, usize) {
 
     (s, k)
 }
+
 
 #[bench]
 fn count_dense(b: &mut Bencher) {
@@ -37,65 +50,6 @@ fn count_dense(b: &mut Bencher) {
 }
 
 #[bench]
-fn count_dense_fast(b: &mut Bencher) {
-    let (s, k) = create_rnd(N, 0.5);
-
-    b.iter(|| {
-        let k2 = s.fast().count();
-        assert_eq!(k, k2);
-    })
-}
-
-#[bench]
-fn copy_sparse(b: &mut Bencher) {
-    let (s, _) = create_rnd(N, 0.1);
-    let mut w = BitSet::with_capacity(N);
-
-    b.iter(|| {
-        for i in s.iter() {
-            w.insert(i);
-        }
-    })
-}
-
-#[bench]
-fn copy_sparse_fast(b: &mut Bencher) {
-    let (s, _) = create_rnd(N, 0.1);
-    let mut w = BitSet::with_capacity(N);
-    
-    b.iter(|| {
-        for i in s.fast() {
-            w.insert(i);
-        }
-    })
-}
-
-#[bench]
-fn copy_dense(b: &mut Bencher) {
-    let (s, _) = create_rnd(N, 0.5);
-    let mut w = BitSet::with_capacity(N);
-
-    b.iter(|| {
-        for i in s.iter() {
-            w.insert(i);
-        }
-    })
-}
-
-#[bench]
-fn copy_dense_fast(b: &mut Bencher) {
-    let (s, _) = create_rnd(N, 0.5);
-    let mut w = BitSet::with_capacity(N);
-    
-    b.iter(|| {
-        for i in s.fast() {
-            w.insert(i);
-        }
-    })
-}
-
-
-#[bench]
 fn count_sparse(b: &mut Bencher) {
     let (s, k) = create_rnd(N, 0.1);
 
@@ -106,11 +60,154 @@ fn count_sparse(b: &mut Bencher) {
 }
 
 #[bench]
-fn count_sparse_fast(b: &mut Bencher) {
-    let (s, k) = create_rnd(N, 0.1);
+fn copy_sparse(b: &mut Bencher) {
+    let (s, _) = create_rnd(N, 0.1);
+    let mut w = BitSet::with_capacity(N);
 
     b.iter(|| {
-        let k2 = s.fast().count();
-        assert_eq!(k, k2);
+        for i in s.iter() { w.insert(i); }
+        black_box(&w);
+    })
+}
+
+#[bench]
+fn copy_dense(b: &mut Bencher) {
+    let (s, _) = create_rnd(N, 0.5);
+    let mut w = BitSet::with_capacity(N);
+
+    b.iter(|| {
+        for i in s.iter() { w.insert(i); }
+        black_box(&w);
+    })
+}
+
+#[bench]
+fn intersect_sparse(b: &mut Bencher) {
+    let (s, _) = create_rnd(N, 0.1);
+    let (s2, _) = create_rnd(N, 0.1);
+
+    b.iter(|| {
+        let k2 = s.intersection(&s2).count();
+        black_box(&k2);
+    })
+}
+
+#[bench]
+fn bench_usize_small(b: &mut Bencher) {
+    let mut r = rng();
+    let mut bit_vec = 0 as usize;
+    b.iter(|| {
+        for _ in 0..100 {
+            bit_vec |= 1 << ((r.next_u32() as usize) % u32::BITS);
+        }
+        black_box(&bit_vec);
+    });
+}
+
+#[bench]
+fn bench_bit_set_big_fixed(b: &mut Bencher) {
+    let mut r = rng();
+    let mut bit_vec = BitVec::from_elem(BENCH_BITS, false);
+    b.iter(|| {
+        for _ in 0..100 {
+            bit_vec.set((r.next_u32() as usize) % BENCH_BITS, true);
+        }
+        black_box(&bit_vec);
+    });
+}
+
+#[bench]
+fn bench_bit_set_big_variable(b: &mut Bencher) {
+    let mut r = rng();
+    let mut bit_vec = BitVec::from_elem(BENCH_BITS, false);
+    b.iter(|| {
+        for _ in 0..100 {
+            bit_vec.set((r.next_u32() as usize) % BENCH_BITS, r.gen());
+        }
+        black_box(&bit_vec);
+    });
+}
+
+#[bench]
+fn bench_bit_set_small(b: &mut Bencher) {
+    let mut r = rng();
+    let mut bit_vec = BitVec::from_elem(u32::BITS, false);
+    b.iter(|| {
+        for _ in 0..100 {
+            bit_vec.set((r.next_u32() as usize) % u32::BITS, true);
+        }
+        black_box(&bit_vec);
+    });
+}
+
+#[bench]
+fn bench_bit_vec_big_union(b: &mut Bencher) {
+    let mut b1 = BitVec::from_elem(BENCH_BITS, false);
+    let b2 = BitVec::from_elem(BENCH_BITS, false);
+    b.iter(|| {
+        b1.union(&b2)
+    })
+}
+
+#[bench]
+fn bench_bit_vec_small_iter(b: &mut Bencher) {
+    let bit_vec = BitVec::from_elem(u32::BITS, false);
+    b.iter(|| {
+        let mut sum = 0;
+        for _ in 0..10 {
+            for pres in &bit_vec {
+                sum += pres as usize;
+            }
+        }
+        sum
+    })
+}
+
+#[bench]
+fn bench_bit_vec_big_iter(b: &mut Bencher) {
+    let bit_vec = BitVec::from_elem(BENCH_BITS, false);
+    b.iter(|| {
+        let mut sum = 0;
+        for pres in &bit_vec {
+            sum += pres as usize;
+        }
+        sum
+    })
+}
+
+#[bench]
+fn bench_bit_vecset_small(b: &mut Bencher) {
+    let mut r = rng();
+    let mut bit_vec = BitSet::new();
+    b.iter(|| {
+        for _ in 0..100 {
+            bit_vec.insert((r.next_u32() as usize) % u32::BITS);
+        }
+        black_box(&bit_vec);
+    });
+}
+
+#[bench]
+fn bench_bit_vecset_big(b: &mut Bencher) {
+    let mut r = rng();
+    let mut bit_vec = BitSet::new();
+    b.iter(|| {
+        for _ in 0..100 {
+            bit_vec.insert((r.next_u32() as usize) % BENCH_BITS);
+        }
+        black_box(&bit_vec);
+    });
+}
+
+#[bench]
+fn bench_bit_vecset_iter(b: &mut Bencher) {
+    let bit_vec = BitSet::from_bit_vec(BitVec::from_fn(BENCH_BITS,
+                                          |idx| {idx % 3 == 0}));
+    b.iter(|| {
+        let mut sum = 0;
+        for idx in &bit_vec {
+            sum += idx as usize;
+        }
+        sum
     })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,97 +1,18 @@
-#![feature(collections)]
 #![feature(core)]
+mod bit;
 
-use std::collections::bit_set::*;
-use std::iter::{Cloned};
-use std::u32;
-use std::slice;
+pub use bit_vec::BitVec;
+pub use bit_set::BitSet;
 
-pub struct FastIter<'a> {
-    head: u32, 
-    head_offset: usize,
-    tail: Cloned<slice::Iter<'a, u32>>
+#[unstable(feature = "collections",
+           reason = "RFC 509")]
+pub mod bit_vec {
+    pub use bit::{BitVec, Iter};
 }
 
-impl<'a> Iterator for FastIter<'a> {
-    type Item = usize;
-
-    #[inline]
-    fn next(&mut self) -> Option<usize> {
-        while self.head == 0 {
-            match self.tail.next() {
-                Some(w) => { 
-                    self.head = w;
-                    self.head_offset += u32::BITS;
-                },
-                None => return None
-            }
-        }
-
-        // let t = self.head & -self.head;
-        let t = self.head & !self.head + 1;
-        // remove the least significant bit
-        self.head &= self.head - 1;
-        // return index of lsb
-        Some(self.head_offset + (u32::count_ones(t-1) as usize))
-    }
-
-    #[inline]
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        match self.tail.size_hint() {
-            (l, Some(h)) => (l * (u32::BITS as usize), Some(1 + h * (u32::BITS as usize))),
-            n => n
-        }
-    }
+#[unstable(feature = "collections",
+           reason = "RFC 509")]
+pub mod bit_set {
+    pub use bit::{BitSet, Union, Intersection, Difference, SymmetricDifference};
+    pub use bit::SetIter as Iter;
 }
-
-pub trait FastBits {
-    fn fast(&self) -> FastIter;
-}
-
-impl FastBits for BitSet {
-
-    fn fast(&self) -> FastIter {
-        let mut bs = self.get_ref().blocks();
-        let x = bs.next().unwrap_or(0);
-
-        FastIter {tail: bs, head: x, head_offset: 0}
-    }
-}
-
-
-#[cfg(test)]
-mod test {
-    
-    extern crate rand;
-    
-    use std::collections::BitSet;
-    use FastBits;
-
-    fn create_rnd(n: usize, p:f32) -> (BitSet, usize) {
-        let mut s = BitSet::with_capacity(n);
-        let mut k = 0;
-
-        for i in 0..n-1 {
-            if rand::random::<f32>() < p {
-                s.insert(i);
-                k += 1;
-            }
-        }
-
-        (s, k)
-    }
-
-    #[test]
-    fn copy() {
-        let (s, _) = create_rnd(10000, 0.5);
-
-        let it1 = s.iter();
-        let it2 = s.fast();
-        for (i, j) in it1.zip(it2) {
-            assert_eq!(i, j);
-        }
-
-    }
-
-}
-
